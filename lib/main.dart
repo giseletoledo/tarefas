3e883f4d852089f093e4d0af78c5d09805868e07
Gisele <gisesonia@gmail.com>
2022-12-05 09:18:12 -0300

    Adicionando a funcionalidade de esconder e mostrar a tela

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool opacidade = true;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,19 +26,51 @@ class MyApp extends StatelessWidget {
             leading: Container(),
             title: const Text('Tarefas'),
           ),
-          body: ListView(
-            children: const [
-              Task(
+          body: AnimatedOpacity(
+            opacity: opacidade ? 1 : 0,
+            duration: Duration(milliseconds: 800),
+            child: ListView(
+              children: const [
+                Task(
                   name: 'Aprender Flutter',
-                  foto: 'https://pbs.twimg.com/media/Eu7e3mQVgAImK2o?format=png&name=large', dificuldade: 3,),
-              Task(name: 'Andar de bike', foto: 'https://3.bp.blogspot.com/-GXGnY7nIpM0/Wd0HYrIJuEI/AAAAAAAAA3E/afmo9jI9H3UwAb9CzjkT4Qt4PHPYOJtUwCLcBGAs/s1600/canstockphoto6512833.jpg', dificuldade: 1,),
-              Task(name: 'Meditar', foto: 'https://catracalivre.com.br/wp-content/uploads/sites/19/2017/05/Medita%C3%A7%C3%A3o-iStock.jpg', dificuldade: 2,),
-              Task(name: 'Ler', foto:'https://www.plannetaeducacao.com.br/portal/arquivo/thumb/artigos/eff9a21305e73bb387a7_990x600_0_0_1_1.png', dificuldade: 4,),
-              Task(name: 'Jogar', foto:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4Tyae_jwwSB9KLE5AKOX7Kl0A5eLN52-Yuw&usqp=CAU', dificuldade: 1,),
-            ],
+                  foto:
+                      'https://pbs.twimg.com/media/Eu7e3mQVgAImK2o?format=png&name=large',
+                  dificuldade: 3,
+                ),
+                Task(
+                  name: 'Andar de bike',
+                  foto:
+                      'https://3.bp.blogspot.com/-GXGnY7nIpM0/Wd0HYrIJuEI/AAAAAAAAA3E/afmo9jI9H3UwAb9CzjkT4Qt4PHPYOJtUwCLcBGAs/s1600/canstockphoto6512833.jpg',
+                  dificuldade: 1,
+                ),
+                Task(
+                  name: 'Meditar',
+                  foto:
+                      'https://catracalivre.com.br/wp-content/uploads/sites/19/2017/05/Medita%C3%A7%C3%A3o-iStock.jpg',
+                  dificuldade: 2,
+                ),
+                Task(
+                  name: 'Ler',
+                  foto:
+                      'https://www.plannetaeducacao.com.br/portal/arquivo/thumb/artigos/eff9a21305e73bb387a7_990x600_0_0_1_1.png',
+                  dificuldade: 4,
+                ),
+                Task(
+                  name: 'Jogar',
+                  foto:
+                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4Tyae_jwwSB9KLE5AKOX7Kl0A5eLN52-Yuw&usqp=CAU',
+                  dificuldade: 1,
+                ),
+              ],
+            ),
           ),
           floatingActionButton: FloatingActionButton(
-            onPressed: () {},
+            onPressed: () {
+              setState(() {
+                opacidade = !opacidade;
+              });
+            },
+            child: Icon(Icons.remove_red_eye),
           ),
         ));
   }
@@ -43,7 +81,11 @@ class Task extends StatefulWidget {
   final String foto;
   final int dificuldade;
 
-  const Task({Key? key, required this.name, required this.foto, required this.dificuldade})
+  const Task(
+      {Key? key,
+      required this.name,
+      required this.foto,
+      required this.dificuldade})
       : super(key: key);
 
   @override
@@ -52,6 +94,7 @@ class Task extends StatefulWidget {
 
 class _TaskState extends State<Task> {
   int nivel = 0;
+
   @override
   Widget build(BuildContext context) {
     //int nivel = 0; Aqui atualiza tod o tempo e fica 0
@@ -61,24 +104,36 @@ class _TaskState extends State<Task> {
         child: Stack(
           children: [
             Container(
-              color: Colors.blue,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4.0),
+                color: Colors.blue,
+              ),
               height: 140,
             ),
             Column(
               children: [
                 Container(
-                  color: Colors.white,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(4.0),
+                    color: Colors.white,
+                  ),
                   height: 100,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Container(
-                        color: Colors.black26,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(4.0),
+                          color: Colors.black26,
+                        ),
                         width: 72,
                         height: 100,
-                        child: Image.network(
-                          widget.foto,
-                          fit: BoxFit.cover,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(4.0),
+                          child: Image.network(
+                            widget.foto,
+                            fit: BoxFit.cover,
+                          ),
                         ),
                       ),
                       Column(
@@ -96,11 +151,41 @@ class _TaskState extends State<Task> {
                               )),
                           Row(
                             children: [
-                              Icon(Icons.star, size: 15, color: (widget.dificuldade >= 1) ? Colors.blue: Colors.blue[100] , ),
-                              Icon(Icons.star,size: 15, color: (widget.dificuldade >= 2) ? Colors.blue: Colors.blue[100],),
-                              Icon(Icons.star,size: 15, color: (widget.dificuldade >= 3) ? Colors.blue: Colors.blue[100],),
-                              Icon(Icons.star,size: 15, color: (widget.dificuldade >= 4) ? Colors.blue: Colors.blue[100],),
-                              Icon(Icons.star,size: 15, color: (widget.dificuldade >= 5) ? Colors.blue: Colors.blue[100],),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.dificuldade >= 1)
+                                    ? Colors.blue
+                                    : Colors.blue[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.dificuldade >= 2)
+                                    ? Colors.blue
+                                    : Colors.blue[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.dificuldade >= 3)
+                                    ? Colors.blue
+                                    : Colors.blue[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.dificuldade >= 4)
+                                    ? Colors.blue
+                                    : Colors.blue[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.dificuldade >= 5)
+                                    ? Colors.blue
+                                    : Colors.blue[100],
+                              ),
                             ],
                           ),
                         ],
@@ -139,7 +224,9 @@ class _TaskState extends State<Task> {
                           width: 200,
                           child: LinearProgressIndicator(
                             color: Colors.white,
-                            value: (widget.dificuldade > 0) ? (nivel/widget.dificuldade) / 10 : 1,
+                            value: (widget.dificuldade > 0)
+                                ? (nivel / widget.dificuldade) / 10
+                                : 1,
                           )),
                     ),
                     Padding(
