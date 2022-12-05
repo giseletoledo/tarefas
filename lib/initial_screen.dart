e27cde307617ef358b81cba3e0caaef11278b9ba
Gisele <gisesonia@gmail.com>
2022-12-05 13:49:34 -0300

    Refatorando separaracao de responsabilidades

---
+++ lib/initial_screen.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+import 'task.dart';
+
+class InitialScreen extends StatefulWidget {
+
+  const InitialScreen({Key? key}) : super(key: key);
+
+  @override
+  State<InitialScreen> createState() => _InitialScreenState();
+}
+
+class _InitialScreenState extends State<InitialScreen> {
+  bool opacidade = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        leading: Container(),
+        title: const Text('Tarefas'),
+      ),
+      body: AnimatedOpacity(
+        opacity: opacidade ? 1 : 0,
+        duration: Duration(milliseconds: 800),
+        child: ListView(
+          children: const [
+            Task(
+              name: 'Aprender Flutter',
+              foto:
+              'https://pbs.twimg.com/media/Eu7e3mQVgAImK2o?format=png&name=large',
+              dificuldade: 3,
+            ),
+            Task(
+              name: 'Andar de bike',
+              foto:
+              'https://3.bp.blogspot.com/-GXGnY7nIpM0/Wd0HYrIJuEI/AAAAAAAAA3E/afmo9jI9H3UwAb9CzjkT4Qt4PHPYOJtUwCLcBGAs/s1600/canstockphoto6512833.jpg',
+              dificuldade: 1,
+            ),
+            Task(
+              name: 'Meditar',
+              foto:
+              'https://catracalivre.com.br/wp-content/uploads/sites/19/2017/05/Medita%C3%A7%C3%A3o-iStock.jpg',
+              dificuldade: 2,
+            ),
+            Task(
+              name: 'Ler',
+              foto:
+              'https://www.plannetaeducacao.com.br/portal/arquivo/thumb/artigos/eff9a21305e73bb387a7_990x600_0_0_1_1.png',
+              dificuldade: 4,
+            ),
+            Task(
+              name: 'Jogar',
+              foto:
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4Tyae_jwwSB9KLE5AKOX7Kl0A5eLN52-Yuw&usqp=CAU',
+              dificuldade: 1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            opacidade = !opacidade;
+          });
+        },
+        child: Icon(Icons.remove_red_eye),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:tarefas/initial_screen.dart';
 
 import 'difficulty.dart';
+import 'task.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +16,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  bool opacidade = true;
 
   @override
   Widget build(BuildContext context) {
@@ -24,191 +25,9 @@ class _MyAppState extends State<MyApp> {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: Scaffold(
-          appBar: AppBar(
-            leading: Container(),
-            title: const Text('Tarefas'),
-          ),
-          body: AnimatedOpacity(
-            opacity: opacidade ? 1 : 0,
-            duration: Duration(milliseconds: 800),
-            child: ListView(
-              children: const [
-                Task(
-                  name: 'Aprender Flutter',
-                  foto:
-                      'https://pbs.twimg.com/media/Eu7e3mQVgAImK2o?format=png&name=large',
-                  dificuldade: 3,
-                ),
-                Task(
-                  name: 'Andar de bike',
-                  foto:
-                      'https://3.bp.blogspot.com/-GXGnY7nIpM0/Wd0HYrIJuEI/AAAAAAAAA3E/afmo9jI9H3UwAb9CzjkT4Qt4PHPYOJtUwCLcBGAs/s1600/canstockphoto6512833.jpg',
-                  dificuldade: 1,
-                ),
-                Task(
-                  name: 'Meditar',
-                  foto:
-                      'https://catracalivre.com.br/wp-content/uploads/sites/19/2017/05/Medita%C3%A7%C3%A3o-iStock.jpg',
-                  dificuldade: 2,
-                ),
-                Task(
-                  name: 'Ler',
-                  foto:
-                      'https://www.plannetaeducacao.com.br/portal/arquivo/thumb/artigos/eff9a21305e73bb387a7_990x600_0_0_1_1.png',
-                  dificuldade: 4,
-                ),
-                Task(
-                  name: 'Jogar',
-                  foto:
-                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4Tyae_jwwSB9KLE5AKOX7Kl0A5eLN52-Yuw&usqp=CAU',
-                  dificuldade: 1,
-                ),
-              ],
-            ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              setState(() {
-                opacidade = !opacidade;
-              });
-            },
-            child: Icon(Icons.remove_red_eye),
-          ),
-        ));
-  }
-}
-
-class Task extends StatefulWidget {
-  final String name;
-  final String foto;
-  final int dificuldade;
-
-  const Task(
-      {Key? key,
-      required this.name,
-      required this.foto,
-      required this.dificuldade})
-      : super(key: key);
-
-  @override
-  State<Task> createState() => _TaskState();
-}
-
-class _TaskState extends State<Task> {
-  int nivel = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    //int nivel = 0; Aqui atualiza tod o tempo e fica 0
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        child: Stack(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(4.0),
-                color: Colors.blue,
-              ),
-              height: 140,
-            ),
-            Column(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(4.0),
-                    color: Colors.white,
-                  ),
-                  height: 100,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(4.0),
-                          color: Colors.black26,
-                        ),
-                        width: 72,
-                        height: 100,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(4.0),
-                          child: Image.network(
-                            widget.foto,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(
-                              width: 200,
-                              child: Text(
-                                widget.name,
-                                style: const TextStyle(
-                                  fontSize: 24,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                              )),
-                          Difficulty(difficultyLevel: widget.dificuldade,),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 52,
-                        width: 52,
-                        child: ElevatedButton(
-                            onPressed: () {
-                              setState(() {
-                                nivel++;
-                              });
-                              print(nivel);
-                            },
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              crossAxisAlignment: CrossAxisAlignment.end,
-                              children: const [
-                                Icon(Icons.arrow_drop_up),
-                                Text(
-                                  'Up',
-                                  style: TextStyle(fontSize: 12),
-                                )
-                              ],
-                            )),
-                      )
-                    ],
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                          width: 200,
-                          child: LinearProgressIndicator(
-                            color: Colors.white,
-                            value: (widget.dificuldade > 0)
-                                ? (nivel / widget.dificuldade) / 10
-                                : 1,
-                          )),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Text(
-                        'Nivel: $nivel',
-                        style:
-                            const TextStyle(color: Colors.white, fontSize: 16),
-                      ),
-                    ),
-                  ],
-                )
-              ],
-            )
-          ],
-        ),
-      ),
+        home: InitialScreen(),
     );
   }
 }
+
+
