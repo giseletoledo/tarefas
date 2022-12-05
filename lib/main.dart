9a91a9fbe0a96edbe02c5c7991429a9876bf2925
Gisele <gisesonia@gmail.com>
2022-12-05 14:03:08 -0300

    Refatorando com diretorios

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:tarefas/initial_screen.dart';
+import 'package:tarefas/screens/initial_screen.dart';
 
-import 'difficulty.dart';
-import 'task.dart';
+import 'components/difficulty.dart';
+import 'components/task.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
